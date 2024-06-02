7b2f33e6b1e048cf0b855c611efb552e83b28ea6
Sudrajat, Rafi | Rafi | MDSD <rafi.sudrajat@rakuten.com>
2024-06-02 21:59:07 +0900

    Complete endpoint for image classification

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
-from typing import Union
 from model import SimpleCNN
 from fastapi import FastAPI, UploadFile, Depends
-import inference
+from Utils import load_model
 from typing_extensions import Annotated
+from PIL import Image
+import io
+from inference import inference
 
 app = FastAPI()
 
-
-@app.get("/binary-classification")
-def read_root():
-    return {"Hello": "World"}
+# Define a global variable to store the model
+model = None
 
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile
-                            #  model:Annotated[dict, Depends(SimpleCNN)]
-                             ):
+def initialize_model() -> SimpleCNN:
+    global model
+    if model is None:
+        model = load_model('artifact/simpleCNN_cat_dog_classifier.pth')
+    return model
+
+
+@app.post("/inference")
+async def running_image_classification(file: UploadFile,
+                             model: Annotated[SimpleCNN,
+                                              Depends(initialize_model)]):
+    # Read the file content
+    contents = await file.read()
+
+    # Convert to PIL Image
+    image = Image.open(io.BytesIO(contents))
+    inference_result = inference(model, image)
     print("file type", type(file.file))
-    return {"filename": file.filename}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return {"inference result": inference_result}
